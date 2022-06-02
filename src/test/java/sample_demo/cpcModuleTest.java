b4cbd819b82dc879c36b2753beeef5dad951494a
akshitsahore <106049885+akshitsahore@users.noreply.github.com>
2022-06-02 16:39:08 +0530

    Update cpcModuleTest.java

--- src/test/java/sample_demo/cpcModuleTest.java
+++ src/test/java/sample_demo/cpcModuleTest.java
@@ -14,7 +14,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class demo {
+public class cpcModuleTest{
 
 	 WebDriver driver;
 	 
